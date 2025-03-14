fd8e2956c9a25ceaf08ebedede77f2065145c7bd
Shakim Harris <amenraprod@gmail.com>
2025-03-14 17:32:40 -0400

    Initial commit

---
+++ src/lessons/lesson1.jsx
@@ -0,0 +1,22 @@
+/* export default function PrintLoop() {
+    for (var i = 0; i < 50; i++) {
+        console.log(i);
+        
+    }
+}
+
+printLoop();
+
+This code shows how var is a function scoped variable, meaning that it is only available within the function in which it is declared.
+*/
+
+/* export default function PrintLoop() {
+    for (let i = 0; i < 50; i++) {      
+    }
+}   console.log(i); // ReferenceError: i is not defined;
+
+printLoop();
+
+This code shows how let is a local scope variable, meaning that it is only available within the block in which it is declared, and cannot be accessed outside of that block.
+*/
+
